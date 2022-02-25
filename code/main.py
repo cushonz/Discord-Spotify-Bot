48f96dacda77a2fdddc6e8116764b9a8d4ffe3f4
Zach <cushonz@cwu.edu>
2022-02-25 15:37:38 -0800

    Token refresh work around

--- code/main.py
+++ code/main.py
@@ -16,18 +16,16 @@ async def on_ready():
 @bot.command(name='rec', help='Adds song to playlist')
 async def stop(ctx):
 	name = ctx.message.content[4:]
-	if Spotify.addToPlaylist(name) != None:
-		await ctx.send("Added "+ name + " to : "+ playlist)
-	else:
-		await ctx.send("Couldn't find '" + name + "', try again.")
+	try:
+		if Spotify.addToPlaylist(name) != None:
+			await ctx.send("Added "+ name + " to : "+ playlist)
+		else:
+			await ctx.send("Couldn't find '" + name + "', try again.")
+	except:
+		os.execv(sys.argv[0], sys.argv)
+		
 
-@bot.command(name='intro', help='Adds song to playlist')
-async def stop(ctx):
-	name = ctx.message.content[4:]
-	if Spotify.addToPlaylist(name) != None:
-		await ctx.send("My job is simple :) I'm basically here just to build a playlist for you!")
-		await ctx.send("Any text following '!rec' will be searched on spotify and added to this playlist : ")
-		await ctx.send(playlist)
+
 		
         
 bot.run('OTQ2NjQzMDE5NjA2Njc5NTky.YhhsAw.jYosZE5fsHuXQ6_IjJwdFKqKju8')
