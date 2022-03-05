087dad06a407b439fa438c943c330b9692b20417
cushonz <cushonz@cwu.edu>
2022-03-04 16:39:12 -0800

    mid step

--- code/main.py
+++ code/main.py
@@ -77,5 +77,17 @@ async def clearQ(ctx):
 	os.system("touch yt_ids.list")
 	await ctx.channel.send("Cleared!")
 
+@bot.command(name='rml', help = 'Removes Last, use if the is no thumbnail on your reccomendation')
+async def rml(ctx):
+	with open('yt_ids.list', 'r+') as file:
+	file.seek(0,os.SEEK_END)
+	pos = file.tell()-1
+	while pos > 0 and file.read(1) !="\n":
+	pos -=1
+	file.seek(pos,os.SEEK_SET)
+
+	if pos > 0:
+		file.seek(pos,os.SEEK_SET)
+		file.truncate()
 
 bot.run(bot_cred['token'])
